ba046341c8f712a2fd30506b70e98e09bac3ad38
Epic10l2 <159151274+Epic10l2@users.noreply.github.com>
2024-05-21 21:46:46 +0200

    Update Bruteforcer.java
    Optimized the code by a lot thanks to the help of 19MisterX98

--- The 32 bit bruteforcer using pillarseeds/src/main/resources/Bruteforcer.java
+++ The 32 bit bruteforcer using pillarseeds/src/main/resources/Bruteforcer.java
@@ -1,17 +1,23 @@
+import java.util.ArrayList;
 import java.util.Random;
-
 public class Bruteforcer{
     public static void main(String[]args){
         long worldseed = 0;
+        long outcount = 0;
+        long startTime = System.currentTimeMillis();
         while (worldseed < 4294967296L) {
-            Random rand = new Random(worldseed);
-            long pillarseed = rand.nextLong() & 65535L;
-            if (pillarseed == 52056) {
+            long randomInternalState1 = (worldseed ^ 25214903917L) % 281474976710656L;
+            long randomInternalState2 = (randomInternalState1 * 25214903917L + 11L) % 281474976710656L;
+            long randomInternalState3 = (randomInternalState2 * 25214903917L + 11L) % 281474976710656L;
+            long nextLongLower32Bits2 = randomInternalState3/65536L;
+            long nextLongOutput2 = nextLongLower32Bits2 * 4294967296L + nextLongLower32Bits2;
+            long pillarseed = nextLongOutput2 % 65536L;
+            if (pillarseed == 39974) {
                 System.out.println(worldseed);
             }
             worldseed = worldseed + 1;
         }
+        long exetime = System.currentTimeMillis() - startTime;
+        System.out.println("Second: " + exetime/1000);
     }
 }
-
-
