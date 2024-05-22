3bd57a292e9ba13f1a343d3bd3f931ad0b45034a
Epic10l2 <159151274+Epic10l2@users.noreply.github.com>
2024-05-22 12:52:55 +0200

    optimization
    Optimited with the help of 19MisterX98

--- The 32 bit bruteforcer using pillarseeds/src/main/resources/Bruteforcer.java
+++ The 32 bit bruteforcer using pillarseeds/src/main/resources/Bruteforcer.java
@@ -5,12 +5,13 @@ public class Bruteforcer{
         long worldseed = 0;
         long outcount = 0;
         while (worldseed < 4294967296L) {
-            long randomInternalState1 = (worldseed ^ 25214903917L) % 281474976710656L;
-            long randomInternalState2 = (randomInternalState1 * 25214903917L + 11L) % 281474976710656L;
-            long randomInternalState3 = (randomInternalState2 * 25214903917L + 11L) % 281474976710656L;
+            long randomInternalState1 = (worldseed ^ 25214903917L) & (281474976710656L-1);
+            long randomInternalState2 = (randomInternalState1 * 25214903917L + 11L) & (281474976710656L-1);
+            long nextLongUpper32Bits2 = randomInternalState2 >> 16;
+            long randomInternalState3 = (randomInternalState2 * 25214903917L + 11L) & (281474976710656L-1);
             long nextLongLower32Bits2 = randomInternalState3 >> 16;
-            long nextLongOutput2 = nextLongLower32Bits2 * 4294967296L + nextLongLower32Bits2;
-            long pillarseed = nextLongOutput2 % 65536L;
+            long nextLongOutput2 = nextLongUpper32Bits2 * 4294967296L + nextLongLower32Bits2;
+            long pillarseed = nextLongOutput2 & 65536-1L;
             if (pillarseed == 39974){
                 System.out.println(worldseed);
             }
